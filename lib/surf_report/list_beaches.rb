2479a242bb98ab51bf9087c24106f8dafc80388f
Richard Steinbach <steinbach.dick@gmail.com>
2017-09-11 14:52:39 -0700

    add beach menu to #list

--- lib/surf_report/list_beaches.rb
+++ lib/surf_report/list_beaches.rb
@@ -8,6 +8,15 @@ class SurfReport::List_beaches
 		puts "1: Venice"
 		puts "2: Malibu"
 		puts "Pick a beach"
+		choice = gets.strip
+
+		case choice
+			when "1"
+			puts "Venice beach report"
+
+			when "2"
+				puts "Malibu beach report"
+		end
 
 	end
 end
