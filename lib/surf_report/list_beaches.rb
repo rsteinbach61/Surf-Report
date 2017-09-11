0059dcb1d0b38525e54f4488e8824982b9ab3585
Richard Steinbach <steinbach.dick@gmail.com>
2017-09-11 15:06:40 -0700

    add loop to menu in #List_beaches.list

--- lib/surf_report/list_beaches.rb
+++ lib/surf_report/list_beaches.rb
@@ -4,19 +4,20 @@ class SurfReport::List_beaches
 	def list
 		#binding.pry
 		SurfReport::Scraper.new.scraping
-		
-		puts "1: Venice"
-		puts "2: Malibu"
-		puts "Pick a beach"
-		choice = gets.strip
+		choice = nil
+		while choice != 'EXIT'
+			puts "1: Venice"
+			puts "2: Malibu"
+			puts "Pick a beach"
+			choice = gets.strip.upcase
 
-		case choice
-			when "1"
-			puts "Venice beach report"
+			case choice
+				when "1"
+				puts "Venice beach report"
 
-			when "2"
-				puts "Malibu beach report"
+				when "2"
+					puts "Malibu beach report"
+			end
 		end
-
 	end
 end
