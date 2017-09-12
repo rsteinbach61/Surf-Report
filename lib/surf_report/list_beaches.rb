e8ecd9b7b6c6b8cffb4c40bb24db2f7e43302c7f
Richard Steinbach <steinbach.dick@gmail.com>
2017-09-11 22:14:29 -0700

    refactor #list #scraping call to split out the region, forcast title, and day from the text/body of the forcast.

--- lib/surf_report/list_beaches.rb
+++ lib/surf_report/list_beaches.rb
@@ -7,18 +7,35 @@ class SurfReport::List_beaches
 		choice = nil
 		while choice != 'EXIT'
 			puts "1: South Los Angeles Forecast"
-			puts "2: Los Angeles Forecast"
+			puts "2: North Los Angeles Forecast"
 			puts "Pick a beach or type EXIT to leave."
 			choice = gets.strip.upcase
 
 			case choice
 				when "1"
-				doc = SurfReport::Scraper.new.scraping('south-los-angeles_2951')	
-				puts doc.css(".forecast-outlook p")[1].text
+					doc = SurfReport::Scraper.new.scraping('south-los-angeles_2951')
+					region = doc.css(".forecast-outlook p strong")[1].text
+					outlook = doc.css(".forecast-outlook p")[1].css("strong")[0].text
+					day = doc.css(".forecast-outlook p")[1].css("strong")[1].text
+					outlook_text = doc.css(".forecast-outlook p")[1].text.gsub(day, '').gsub(outlook, '')
+					puts region
+					puts outlook
+					puts day	
+					puts outlook_text
+
 
 				when "2"
 					doc = SurfReport::Scraper.new.scraping('north-los-angeles_2142')	
-				puts doc.css(".forecast-outlook p")[1].text
+					region = doc.css(".forecast-outlook p strong")[1].text
+					outlook = doc.css(".forecast-outlook p")[1].css("strong")[0].text
+					day = doc.css(".forecast-outlook p")[1].css("strong")[1].text
+					outlook_text = doc.css(".forecast-outlook p")[1].text.gsub(day, '').gsub(outlook, '')
+					
+					puts region
+					puts outlook
+					puts day	
+					puts outlook_text
+
 			end
 		end
 	end
